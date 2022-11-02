0c60157b177cbb3d4e85a60d4700d9ea200afb05
MuhammadArbiIIT <marbi@hawk.iit.edu>
2022-11-02 17:05:00 -0500

    create function check inputs

--- js/site.js
+++ js/site.js
@@ -1,9 +1,18 @@
 const form1 = document.getElementById('form1')
-const name = document.getElementById('name')
+const name = document.getElementById('fullname')
 const phone = document.getElementById('phone')
+const email = document.getElementById('email')
 
 form1.addEventListener('submit', (e) => {
     e.preventDefault();
 
     checkInputs();
 });
+
+function cehckInputs() {
+    //get values
+    const fullnameVaule = fullname.value
+    const phoneVaule = phone.value
+    const emailVaule = email.value
+
+}

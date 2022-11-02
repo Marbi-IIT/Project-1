e6b2745ea4110d58dcab4b4c80f5731f6f1e2e21
MuhammadArbiIIT <marbi@hawk.iit.edu>
2022-11-02 17:35:25 -0500

    fixed JS error message

--- js/site.js
+++ js/site.js
@@ -1,18 +1,11 @@
-const form1 = document.getElementById('form1')
-const name = document.getElementById('fullname')
-const phone = document.getElementById('phone')
-const email = document.getElementById('email')
+function checkempty() {
+    
+    var fname = document.forms["form1"]["fullname"].value
+    var phone = document.forms["form1"]["phone"].value
+    var email = document.forms["form1"]["email"].value
 
-form1.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    checkInputs();
-});
-
-function cehckInputs() {
-    //get values
-    const fullnameVaule = fullname.value
-    const phoneVaule = phone.value
-    const emailVaule = email.value
-
+    if((!fullname) || (!phone) || (!email) ) {
+        alert("Please fill out the required fields");
+        return false;
+    }
 }

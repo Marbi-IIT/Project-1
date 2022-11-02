1d036d2fda2d22f6f71e0b9ab882fda3515c66e1
MuhammadArbiIIT <marbi@hawk.iit.edu>
2022-11-02 15:52:40 -0500

    change media query height and change form to fit JS

--- js/site.js
+++ js/site.js
@@ -1,16 +1,20 @@
 const name = docment.getElementByID('name')
-const password = document.getElementById('password')
-const form = document.getElementById('form')
+const phonenumber = document.getElementById('phone')
+const form = document.getElementById('email')
 const errrorEleemnt = document.getElementById('error')
 
 form.addEventListener('submit', (e) => {
     let messages = []
-    if (name.value === '' || name.value == null) {
+    if (Text.value === '' || Text.value == null) {
         messages.push('Name is required')
+    }
+
+    if (phonenumber.value.lenght <= 10) {
+        messages.push('Phone number must be (---,---,----)')
     }
 
     if (messages.length > 0) {
         e.preventDefault()
-        error.Element.innerText = messages.join(',')
+        error.Element.innerText = messages.join(', ')
     }
-}
+})
